4cbe69b9f96354fc079059a63fb356e02393e9bc
Lillian <weijingwen35@foxmail.com>
2019-12-12 12:00:19 +0800

    使用styled-components整理样式

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { GlobalStyle } from './style.js';
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       hello world
     </div>
   );
